81c47d03f25b8bad34e0c280c35c48aebcef7d93
Dongjin Kuk <puter2712goo@gmail.com>
2025-08-19 17:46:50 +0900

    Remove unnecessary variable

--- src/core/sprite_renderer.h
+++ src/core/sprite_renderer.h
@@ -16,9 +16,6 @@ public:
 
     void Render();
 
-    const glm::mat4& GetTransform() const { return _transform; }
-    void SetTransform(const glm::mat4& transform) { _transform = transform; }
-
 private:
     unsigned char* _img;
     GLuint _texture;
@@ -26,7 +23,5 @@ private:
     GLuint _vao;
     GLuint _vbo;
     GLuint _ebo;
-
-    glm::mat4 _transform = glm::mat4(1.0f);
 };
 } // namespace uray
